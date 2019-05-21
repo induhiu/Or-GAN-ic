ea6c57ecb1da6bf8d0a5185e1fd0c78e3f771396
Kenny Talarico <kennytalarico@gmail.com>
2019-05-21 14:38:04 -0400

    commented and changed some parameters

--- master.py
+++ master.py
@@ -9,38 +9,44 @@ import numpy as np
 
 class Model():
 
-    def __init__(self, in=None, out=None):
-        self.in = in
-        self.out = out
-
-
+    def __init__(self):
+        pass
 
 class Generator(Model):
 
-    def __init__(self):
+    def __init__(self, in):
         Model.__init__()
+        self.size = in.shape
 
     def generate(din=None):
-        self.out = (np.random.rand(self.in.shape[1], self.in.shape[0]) if din is None else self.something(din))
-
+        # din is the discriminator input. If din is none this means that this is
+        # the first time that we're calling generate and so the ouput should be
+        # randomized. Otherwise, do something with din. We'll figure that out later.
+        self.out = (np.random.rand(self.size[1], self.size[0]) if din is None else self.something(din))
 
 
 class Discriminator(Model):
 
-    def __init__(self):
+    def __init__(self, out):
         Model.__init__()
+        self.out = out
 
     def discriminate(in):
         pass
 
 
 def main():
+    # test input and output
     samplein = np.array([[0,0,1],
                          [0,1,1],
                          [1,0,1],
                          [1,1,1]])
+    sampleout = np.array([[0],[1],[1],[0]])
+    sampleout = sampleout.reshape(4, 1)
+
+    # Here through the for loop should always be here
     G = Generator(samplein)
-    D = Discriminator()
+    D = Discriminator(sampleout)
 
     G.generate()
     D.discriminate(G.out)
