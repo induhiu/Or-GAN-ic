cd5e7929fcef718f927f7f3cb5513f1fd4ede76e
Kenny Talarico <kennytalarico@gmail.com>
2019-05-21 13:48:56 -0400

    original framework

---
+++ master.py
@@ -0,0 +1,34 @@
+"""
+CREATING A LANGUAGE WITH COLLABORATIVE NEURAL NETWORKS
+Ian Nduhiu and Kenny Talarico, advisor David Perkins
+Hamilton College
+Summer 2019
+"""
+
+class Model():
+
+    def __init__(self):
+        pass
+
+
+
+class Generator(Model):
+
+    def __init__(self):
+        Model.__init__()
+
+class Discriminator(Model):
+
+    def __init__(self):
+        Model.__init__()
+
+
+
+
+def main():
+    G = Generator()
+    D = Discriminator()
+
+
+if __name__ == "__main__":
+    main()
