118204860b50d3569f789ffdc2ee98c2a4955806
Kenny Talarico <kennytalarico@gmail.com>
2019-05-23 10:45:48 -0400

    fized createBits

--- mom.py
+++ mom.py
@@ -6,34 +6,34 @@ Implementation by Kenny Talarico, 5/23/2019.
 """
 import numpy as np
 
-def createBits(x):
+def createBits(x, num):
     result = "";
     # x is length of bitstring
-    v = 2 ** x
+    v = 2 ** (x - 1)
     for _ in range(x):
-      if x >= v:
-        result += '1'
-        x -= v
+      if num >= v:
+          result += '1'
+          num -= v
       else:
           result += '0'
       v /= 2
     return result
 
 def createLogograms(A):
-    # change to alter dimensions of 3x3 array
+    # change to alter dimensions of 3x3 array. dim[0] and dim[1] should be
+    # the same.
     dim = (3, 3)
 
     size = dim[0] * dim[1]
 
     # l is a list of empty NumPy arrays of the above dimensions
-    l = [np.empty((3, 3), dtype=int) for _ in range 2 ** size]
+    l = [np.empty((3, 3), dtype=int) for _ in range(2 ** size)]
 
+    # create every permutation of 0s and 1s
     for x in range(2 ** (dim[0] * dim[1])):
-        for ch in createBits(size):
-            for a in range(dim[0]):
-                for b in range(dim[1]):
-                    l[x][a][b] = ch
-
+        bits = createBits(size, x)
+        for ch in range(len(bits)):
+            l[x][ch // dim[0]][ch % dim[0]] = int(bits[ch])
     return l
 
 def main():
@@ -42,7 +42,12 @@ def main():
 
     signs = ['red', 'green', 'blue']
 
-    logograms = createLogograms(alphabet)
+    # logograms = createLogograms(alphabet)
+
+    # print(logograms[511])
+
+    for x in range(256):
+        print(createBits(8, x))
 
 if __name__ == '__main__':
     main()
