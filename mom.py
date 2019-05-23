284bcad78a7dc020077f7db9cddc1e109c542803
Kenny Talarico <kennytalarico@gmail.com>
2019-05-23 14:19:01 -0400

    updated to allow for any 2 character alphabet

--- mom.py
+++ mom.py
@@ -5,7 +5,7 @@ to represent various English words.
 Implementation by Kenny Talarico, 5/23/2019.
 """
 import numpy as np
-from random import choice as pick
+import secrets
 
 class Logogram:
 
@@ -25,43 +25,46 @@ class Mom:
 
     def _createLanguage(self):
 
-        # l is a list of empty Logograms with NumPy arrays of the above dimensions
-        logo = [Logogram(np.empty((3, 3), dtype=int)) for _ in range(2 ** self.size)]
+        self.dictionary = {}
+        used = []
+        for s in self.signs:
+            n = secrets.randbelow(2 ** self.size)
+            used.append(n)
+            # avoid overlap
+            while n in used: n = secrets.randbelow(2 ** self.size)
 
-        # create every permutation of 0s and 1s
-        for x in range(2 ** self.size):
-            # change to reflect whatever characters are in alphabet
-            bits = createBits(self.size, x)
+            logo = Logogram(np.empty(self.dim, dtype=str))
+            bits = createBits(self.size, n, self.alphabet)
             for ch in range(len(bits)):
-                logo[x].L[ch // self.dim[0]][ch % self.dim[0]] = int(bits[ch])
+                logo.L[ch // self.dim[0]][ch % self.dim[0]] = bits[ch]
 
-        self.dictionary = {l:pick(self.signs) for l in logo}
+            self.dictionary[logo] = s
 
     def __str__(self):
         for ary in self.dictionary:
             print(ary.L, self.dictionary[ary])
-        return("--MOM--" + '\n' + "My alphabet consists of " + str(len(self.alphabet)) +
-               " characters: " + str(self.alphabet) + '. ' + "I have " + str(2**self.size) +
-               " words for " + str(self.signs) + '.')
+        return("My alphabet consists of " + str(len(self.alphabet)) + " characters: "
+               + str(self.alphabet) + '. ' + "I have " + str(len(self.signs)) +
+               " signs: " + str(self.signs) + '.')
 
 
-def createBits(x, num):
+def createBits(x, num, al):
     result = "";
     # x is length of bitstring
     v = 2 ** (x - 1)
     for _ in range(x):
       if num >= v:
-          result += '1'
+          result += str(al[1])
           num -= v
       else:
-          result += '0'
+          result += str(al[0])
       v /= 2
     return result
 
 
 def main():
     # Change to fill array with any other values
-    mom = Mom([0, 1], ['red', 'green', 'blue'])
+    mom = Mom(['%', '$'], ['red', 'green', 'blue'])
     print(mom)
 
 
