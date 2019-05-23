b3303aec4474f62e9cf14bdc3a270cdd429e9919
Kenny Talarico <kennytalarico@gmail.com>
2019-05-23 09:21:58 -0400

    shell for mom

---
+++ mom.py
@@ -0,0 +1,12 @@
+"""
+Early implementation of a "mom" class. The "mom" invents a "language" and
+teaches it to the "baby." This "mom" uses NumPy arrays of zeros and ones to
+represent various English words.
+Implementation by Kenny Talarico, 5/23/2019.
+"""
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
