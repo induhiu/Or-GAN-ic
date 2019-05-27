768caaebb21938a90873a073d06e195cb1a76022
Kenny Talarico <kennytalarico@gmail.com>
2019-05-27 10:01:08 -0400

    new file to implement mom decision without neural

---
+++ momdecide.py
@@ -0,0 +1,10 @@
+""" Attempt to implement the "mom" language consensus without using a neural
+network. Implementation by Kenny Talarico, May 2019. """
+
+import mom
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
