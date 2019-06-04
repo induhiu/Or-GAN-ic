85250782e85cd1f4e2056942757c83b9b793a71b
P-Rhyme <42650290+P-Rhyme@users.noreply.github.com>
2019-06-04 16:51:31 -0400

    Cleaned up dead code, and commented fully.

--- bezier.py
+++ bezier.py
@@ -1,34 +1,48 @@
+"""
+DATE: June 4, 2019
+FILE: bezier.py
+AUTH: Dave Perkins
+
+DESC: This program helps generate a set of characters for the forest's
+      'common language'. The squiggles are meant to look like chemical
+      signatures. We want to use the program to create ten characters
+      and 7000 similar copies of each, to mimic the MNIST database.
+"""
+
 import numpy as np
 from scipy.special import comb
 from matplotlib import pyplot as plt
 from sys import exit
 from PIL import Image
 
-# GLOBAL CONSTANTS:
-MY_DPI = 255
-MAX_COORD = 28
-NUM_CURVES = 3
-NUM_POINTS_IN_CURVE = 4
+# Our created images will be MAX by MAX pixels in size:
+MAX = 28
+
+# Once the user picks a character, we generate this many similar characters:
+NUM_CHARACTERS_IN_FAMILY = 20
+
+# The Bezier curves (the squiggles) require points, and I want to space them
+# out a bit, so I'm defining 'quadrants' from which we will choose these
+# points. To get more spacing, the quadrants are really the four corners
+# of the image's square.
+
+# Make this larger to increase the sizes of these corner squares:
+width = 5
 
 # Quadrants are measured from lower left to upper right:
-Q0 = []
-# Q1 = [(MAX_COORD // 2, MAX_COORD // 2), (MAX_COORD, MAX_COORD)]
-# Q2 = [(0, MAX_COORD // 2), (MAX_COORD // 2, MAX_COORD)]
-# Q3 = [(0, 0), (MAX_COORD // 2, MAX_COORD // 2)]
-# Q4 = [(MAX_COORD // 2, 0), (MAX_COORD, MAX_COORD // 2)]
-Q1 = [(18, 18), (22, 22)]
-Q2 = [(1, 18), (5, 22)]
-Q3 = [(1, 1), (5, 5)]
-Q4 = [(18, 1), (22, 5)]
+Q0 = [] # for index fixing -- just ignore!
+Q1 = [(MAX - width, MAX - width), (MAX, MAX)]
+Q2 = [(0, MAX - width), (width, width)]
+Q3 = [(0, 0), (width, width)]
+Q4 = [(MAX - width, 0), (MAX, width)]
+
 QUADRANTS = [Q0, Q1, Q2, Q3, Q4]
 
 
 def bernstein_poly(i, n, t):
-    """
-     The Bernstein polynomial of n, i as a function of t
-    """
+    """ Returns the Bernstein polynomial of n, i as a function of t. """
 
-    return comb(n, i) * ( t**(n-i) ) * (1 - t)**i
+    return comb(n, i) * ( t ** (n - i) ) * (1 - t) ** i
 
 
 def bezier_curve(points, nTimes=1000):
@@ -39,7 +53,7 @@ def bezier_curve(points, nTimes=1000):
        points should be a list of lists, or list of tuples
        such as [ [1,1],
                  [2,3],
-                 [4,5], ..[Xn, Yn] ]
+                 [4,5], ... [Xn, Yn] ]
         nTimes is the number of time steps, defaults to 1000
 
         See http://processingjs.nihongoresources.com/bezierinfo/
@@ -51,7 +65,9 @@ def bezier_curve(points, nTimes=1000):
 
     t = np.linspace(0.0, 1.0, nTimes)
 
-    polynomial_array = np.array([bernstein_poly(i, nPoints-1, t) for i in range(0, nPoints)])
+    polynomial_array = np.array([bernstein_poly(i, nPoints-1, t)
+                                 for i in range(0, nPoints)
+                                ])
 
     xvals = np.dot(xPoints, polynomial_array)
     yvals = np.dot(yPoints, polynomial_array)
@@ -66,9 +82,6 @@ def pick_points(quads):
         quads is a list of quadrants, like [1, 2, 4, 3, 1], from which we
         will choose our random points (in the given order).
 
-        NUM_CURVES is the number of lists;
-        NUM_POINTS_IN_CURVE is the number of tuples in each list.
-
         Like this:
 
         [ [(2, 4), (7, 2), (3, 4), (1, 9)] ,
@@ -77,33 +90,7 @@ def pick_points(quads):
         ]
     """
 
-    # results = []
-    #
-    # for _ in range(NUM_CURVES):
-
-    lst = []
-
-    for quad in quads:
-        x, y = get_point_from_quadrant(QUADRANTS[quad])
-        lst.append((x, y))
-
-    return(lst)
-
-    # results.append(lst)
-
-    # return results
-
-    # # This works for random points:
-    # result = [np.random.rand(NUM_POINTS_IN_CURVE, 2) * MAX_COORD]
-    # print(result)
-    #
-    # for row in range(NUM_CURVES - 1):
-    #     lst = [result[row - 1][-1]]
-    #     extra = np.random.rand(NUM_POINTS_IN_CURVE - 1, 2) * MAX_COORD
-    #     r = np.concatenate((lst, extra), axis = 0)
-    #     result.append(r)
-    #
-    # return result
+    return [(get_point_from_quadrant(QUADRANTS[quad])) for quad in quads]
 
 
 def get_point_from_quadrant(quad):
@@ -122,15 +109,9 @@ def get_point_from_quadrant(quad):
 def define_lines(num_lines):
     """ Returns num_lines lists of quadrants from which to choose points. """
 
-    quad_list = []
-
-    for _ in range(num_lines):
-
-        # Choose a random number of quadrants:
-        q = np.random.randint(1, 5, np.random.randint(4, 8))
-        quad_list.append(q)
-
-    return quad_list
+    return [np.random.randint(1, 5, np.random.randint(4, 8))
+            for _ in range(num_lines)
+           ]
 
 
 def create_character(fname, quad_list):
@@ -145,58 +126,50 @@ def create_character(fname, quad_list):
     # Turn the axes off so that we only see and save the image:
     ax.set_axis_off()
 
-    points_list = []
-
-    for q in quad_list:
-
-        # Grab NUM_POINTS_IN_CURVE points for each of the NUM_CURVES curves:
-        points_list.append(pick_points(q))
+    # This list of lists stores all of the points, chosen from the various
+    # quadrants, for each of the bezier curves we wish to draw:
+    points_list = [pick_points(q) for q in quad_list]
 
     # Graph all of the curves:
     for lst in points_list:
 
-        xvals, yvals = bezier_curve(lst, nTimes=1000)
+        # Calculate a list of points to connect for each curve:
+        xvals, yvals = bezier_curve(lst, nTimes=200)
 
-        # solid_capstyle is used for capping line endpoints:
+        # Plot; use solid_capstyle for rounding line endpoints:
         plt.plot(xvals, yvals, 'k', linewidth = 2, solid_capstyle = 'round')
 
-    # Some characteristics of the graph:
-    plt.xlim(0, 28)
-    plt.ylim(0, 28)
+    # Set the plot's axes to match the desired image dimensions:
+    plt.xlim(0, MAX)
+    plt.ylim(0, MAX)
 
-    # Save the figure:
-    plt.savefig(fname, dpi = 28)
+    # Save the figure (in color):
+    plt.savefig(fname, dpi = MAX)
     plt.close()
 
-    # Convert to reversed grayscale, so the writing is white, background black:
-    # with Image.open(fname).convert("LA") as image:
-    #     arr = np.asarray(image)
-    #     plt.imsave(fname, arr, cmap = 'gray_r', dpi = 28)
-
+    # Convert to grayscale:
     Image.open(fname).convert('L').save(fname)
 
 
 def main():
     """ The main function. """
 
-    asking = True
+    # A flag that allows the user to view multiple characters before picking:
+    filename = ''
 
-    while asking:
+    while filename == '':
 
         # Pick a random number of lines to use, and generate quadrants for each:
-        quad_list = define_lines(np.random.randint(2, 5))
+        quad_list = define_lines(np.random.randint(2, 4))
 
         # Make one character to see if it's a keeper:
         create_character('test', quad_list)
 
-        filename = input()
+        # If the user hits Enter, we continue generating test characters:
+        filename = input('Enter a filename to keep this character:')
 
-        if filename != '':
-            asking = False
-
-    for pic in range(20):
-
-        # Create a new character:
+    # Create the appropriate number of similar characters to the user's choice:
+    for pic in range(NUM_CHARACTERS_IN_FAMILY):
         create_character(filename + str(pic), quad_list)
 
 
