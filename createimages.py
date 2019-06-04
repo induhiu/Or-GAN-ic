497cd707e96f4435d95821ed6d39b2902a042ab5
Kenny Talarico <kennytalarico@gmail.com>
2019-06-04 16:13:18 -0400

    minor updates

--- createimages.py
+++ createimages.py
@@ -6,16 +6,17 @@ from numpy import array
 from PIL import Image
 
 def writetofile(ary, out):
-    out.write('\n'.join(' '.join(str(c) for c in r) for r in ary))
+    out.write('\n'.join(' '.join(str(255-c) for c in r) for r in ary))
+    out.write('\n')
 
 def main():
-    ary = np.array(Image.open('testin.png'))
+    ary = array(Image.open('testin.png'))
     out = open('testout.txt', 'w')
 
-    writetofile(ary, out)
-    writetofile(ary, out)
+    for _ in range(70000):
+        writetofile(ary, out)
 
-    Image.fromarray(ary).save('testout.png')
+    #Image.fromarray(ary).save('testout.png')
 
-if __name__ == '__main__'
+if __name__ == '__main__':
     main()
