f38dfe3a9786c6b00fff8217437b57698bed4ca6
Kenny Talarico <kennytalarico@gmail.com>
2019-06-07 11:34:39 -0400

    testing

--- master.py
+++ master.py
@@ -4,13 +4,24 @@
     Summer 2019 """
 
 import numpy as np
-import forest
+# import forest
 import gan
 
+def slicedata(dic, size):
+    vals = list(dic.values())
+    li1 = vals[:size]
+    li2 = vals[size:]
+    return (li1, li2)
 
 def main():
-    pass
+    xtrain, xtest = slicedata(np.load('imgarys.npz'), 60000)
+    xtrain = np.array(xtrain)
+    xtest = np.array(xtest)
+    print(xtrain.shape)
+    print(xtest.shape)
 
+    ganny = gan.GAN(100, x_train=xtrain, x_test=xtest)
+    ganny.train(10)
 
 if __name__ == "__main__":
     main()
