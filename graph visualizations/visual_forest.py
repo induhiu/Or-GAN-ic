1d0b48601ee1b3e81cb8d535c46fde9d075794ee
Ananab Tilps <ananab.tilps@gmail.com>
2019-06-13 11:17:25 -0400

    Ready to import into Kenny's.

--- graph visualizations/visual_forest.py
+++ graph visualizations/visual_forest.py
@@ -7,42 +7,83 @@
          3: [(0,10), [1], 20]}
 
          id_number: [coordinates, list of neighbors, radius/age]
+
+    d = {(0,0): [(1,1), (2,3)],
+         (1,1): [(0,0)],
+         (2,3): [(0,0)]
+         }
+
+    locations = [<tree object>, <tree object>, <tree object>]
+
+
+
 """
 
 import networkx as nx
 import matplotlib.pyplot as plt
-import sys
-import pickle
-from matplotlib.axes._axes import _log as matplotlib_axes_logger
+# import sys
+# import pickle
+# from matplotlib.axes._axes import _log as matplotlib_axes_logger
 
 
-def generate_graph():
-    """ Returns a graph with desired characteristics. """
+# def generate_graph():
+#     """ Returns a graph with desired characteristics. """
+#
+#     # nodes:
+#     positions = {i: forest.trees[i].location
+#                  for i in range(len(forest.trees))
+#                  if forest.trees[i]
+#                 }
+#
+#     # edges:
+#     d = {(0,0): [(1,1), (2,3)],
+#          (1,1): [(0,0)],
+#          (2,3): [(0,0)]
+#          }
+#
+#     # positions = {0: (0, 0), 1: (1, 1), 2: (1, 2), 3: (2, 1), 4: (2, 2)}
+#     # G = nx.Graph(5, 20, pos = positions)
+#     G = nx.random_geometric_graph(5, 20, pos = positions)
+#     # edges = [(0, 1), (1, 2)]
+#     return G, positions, edges
 
-    positions = {0: (0, 0), 1: (1, 1), 2: (1, 2), 3: (2, 1), 4: (2, 2)}
-    # G = nx.Graph(5, 20, pos = positions)
-    G = nx.random_geometric_graph(5, 20, pos = positions)
-    edges = [(0, 1), (1, 2)]
-    return G, positions, edges
-
-def main():
+def graph(trees):
     """ The main function. """
 
     # This represses some weird warnings that I was getting:
-    matplotlib_axes_logger.setLevel('ERROR')
+    # matplotlib_axes_logger.setLevel('ERROR')
 
     # Read the dictionary from the data file:
-    with open('testdata.txt', 'rb') as fn:
-        dct = pickle.loads(fn.read())
+    # with open('testdata.txt', 'rb') as fn:
+    #     dct = pickle.loads(fn.read())
 
     # Create the graph:
     G = nx.Graph()
 
-    # Get various data structures from the dictionary:
-    coords = {key: value[0] for key, value in dct.items()}
-    edges = [(k, e) for k, v in dct.items() for e in v[1] if e > k]
-    radii = [value[2] for value in dct.values()]
-    labels = {key: value[0] for key, value in dct.items()}
+    ### Coordinates of the nodes:
+
+    # previous:
+    # coords = {key: value[0] for key, value in dct.items()}
+
+    # new:
+    coords = {trees[i]: trees[i].location
+                               for i in range(len(trees))
+                               if trees[i]
+                               }
+
+    ### Edges:
+
+    # previous:
+    # edges = [(k, e) for k, v in dct.items() for e in v[1] if e > k]
+    # edges = [(1,2), (1,3)]
+
+    # new:
+    edges = [(t, e) for t in coords for e in t.neighbors]
+
+
+
+    # radii = [value[2] for value in dct.values()]
+    # labels = {key: value[0] for key, value in dct.items()}
 
     for edge in edges:
         G.add_edge(edge[0], edge[1])
@@ -62,17 +103,17 @@ def main():
 
     nx.draw_networkx_nodes(G,
                            coords,
-                           node_size = radii,
+                           # node_size = radii,
                            node_color = 'g',
                            alpha = 0.9
                            )
 
-    nx.draw_networkx_labels(G,
-                            coords,
-                            labels,
-                            font_size = 24,
-                            font_color = 'b',
-                            alpha = 0.8)
+    # nx.draw_networkx_labels(G,
+    #                         coords,
+    #                         labels,
+    #                         font_size = 24,
+    #                         font_color = 'b',
+    #                         alpha = 0.8)
 
     # plt.axis('off')
     plt.show()
