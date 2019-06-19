eda81bd81bf930006cf9a305309a46eb644d3c50
induhiu <induhiu@hamilton.edu>
2019-06-19 09:13:39 -0800

    gan

--- gan.py
+++ gan.py
@@ -22,6 +22,7 @@ from tensorflow.keras.models import load_model
 import language_getter
 from pickle import load
 from random import shuffle
+from collections import Counter
 
 
 # Let Keras know that we are using tensorflow as our backend engine
@@ -32,6 +33,9 @@ np.random.seed(10)
 
 # The dimension of our random noise vector.
 random_dim = 100
+
+# # Load a neural network
+# my_nn = load_model('nn.h5')
 
 class Generator:
     def __init__(self, optimizer=Adam(lr=0.0002, beta_1=0.5), g=None):
@@ -90,7 +94,7 @@ class GAN:
                          metrics=['accuracy'])
 
     def train(self, epochs=1, batch_size=128, id=1, plot=True,
-            attack=False, all_generated_images=[]):
+            attack=False):
         # Get the training and testing data
         x_train, y_train, x_test, y_test = 0, 0, 0, 0
         if self.curr_x_train is not None:
@@ -131,21 +135,19 @@ class GAN:
                 # # -----------------------------------------------------#
                 # # Experience replay algorithm(still in testing)
                 # # Comment out if you intend to use normal gan
-                # # Create an interval
-                interval = 10
+                # # Create an interval larger than 1
+                interval = 3
                 if e % interval == 0:
-                    # get the four most recent generations and reshape
-                    # array will be in shape (4, 128, 784), will need reshaping
-                    x = np.array(old_imgs[-4:]).reshape(128*4, 784)
+                    # get the most recent generations and reshape accordingly
+                    x = np.array(old_imgs[-2:]).reshape(200*2, 784)
                     # shuffle the array
                     shuffle(x)
 
-                    # Randomly select images to use. Make sure number of images
-                    # is 25 % of total generated images we will pass
-                    experience_rep = np.array([img for img in x[:32]])
+                    # Select images to use.
+                    experience_rep = np.array([img for img in x[:10]])
 
-                    # Combine recently generated images(75%) and old ones(25%)
-                    gen_images = np.array(list(generated_images)[:96] + \
+                    # Combine recently generated images and old ones
+                    gen_images = np.array(list(generated_images)[:118] + \
                                 list(experience_rep))
 
                     # Concatenate the fake images and real ones
@@ -174,20 +176,29 @@ class GAN:
                     else None
             # Plots the images if plot is set to True(default)
             # Can add an extra condition e.g. if id == 10
-            if plot and id == 11:
-                all_generated_images.append(plot_generated_images(id, self.G))
+            if plot:
+                plot_generated_images(id, self.G)
+
+            # # ----------------------------------------------- #
+            # # Counting the images using a neural network
+            # # Create new noise
+            # noise = np.random.normal(0, 1, size=(batch_size, random_dim))
+            # # View the count
+            # print(get_count(self.G.predict(noise)))
+            # # Press enter to continue once its done
+            # input()
+            #
+            # # ----------------------------------------------- #
+
 
             # Append recent generations to old images list
             # Realized the bug could be that they are in different formats
             # Trying out sth new. Will style up the code
-            for _ in range(2):
-                noise = np.random.normal(0, 1, size=[batch_size, random_dim])
-                to_be_old_imgs = self.G.predict(noise)
-                old_imgs.append(to_be_old_imgs)
-
+            imgs = language_getter.produce_language(self.G, n=2).reshape(200,784)
+            imgs = (imgs.astype(np.float32) - 127.5)/127.5
+            old_imgs.append(imgs)
             # Increase id
             id += 1
-        return all_generated_images
 
     def toggleDTrain(self):
         self.D.trainable = not self.D.trainable
@@ -228,24 +239,16 @@ def plot_generated_images(id, generator, examples=100, dim=(10, 10),
         copy += 1
     plt.savefig(newfile)
     plt.close('all')
-    return generated_images
+
+# def get_count(data):
+#     ''' Uses the neural network to get counts '''
+#     pred = my_nn.predict(data)
+#     return Counter(['ABCDEFGHIJ'[list(x).index(max(x))] for x in pred])
+
 #
 if __name__ == '__main__':
 # #     # GAN().train(epochs=20)
     vals = np.array(load(open('lang_for_gan.txt', 'rb'))[:60000])
-    for _ in range(10):
-        gen, disc = Generator(), Discriminator()
-        my_gan = GAN(generator=gen, discriminator=disc, x_train=vals)
-        my_gan.train(epochs=11)
-
-
-        # print(gen, disc)
-        # my_gan = GAN(x_train=vals, generator=gen, discriminator=disc)
-        # my_gan.train(epochs=6)
-#     gan2 = GAN(x_train=language_getter.produce_language(my_gan.G))
-#     gan2.train(epochs=10)
-#     gan3 = GAN(x_train=language_getter.produce_language(gan2.G))
-#     gan3.train(epochs=10)
-#     # with open('counter.txt', 'wb') as file:
-#         # pickle.dump(gan.train(epochs=10, plot=False), file)
-#     # gan.train(epochs=10, plot=False)
+    gen, disc = Generator(), Discriminator()
+    my_gan = GAN(x_train=vals, generator=gen, discriminator=disc)
+    my_gan.train(epochs=10)
