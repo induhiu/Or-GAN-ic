188217c1aa542018d93e08d393a310acdb2de064
induhiu <induhiu@hamilton.edu>
2019-06-24 12:54:10 -0800

    gan stuff

--- gan.py
+++ gan.py
@@ -304,10 +304,10 @@ if __name__ == '__main__':
     ganny2 = GAN(x_train=vals)
     ganny3 = GAN(x_train=vals, generator=ganny1.G, discriminator=ganny2.D)
     ganny4 = GAN(x_train=vals, generator=ganny2.G, discriminator=ganny1.D)
-    # ganny1.train(epochs=30)
-    epochs = 10
-    for _ in range(epochs):
-        ganny1.train()
-        ganny2.train()
-        ganny3.train()
-        ganny4.train()
+    ganny1.train(epochs=40)
+    # epochs = 10
+    # for _ in range(epochs):
+    #     ganny1.train()
+    #     ganny2.train()
+    #     ganny3.train()
+    #     ganny4.train()
