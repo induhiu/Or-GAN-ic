1617c1cee619c158e5d4461316e55ba554774661
induhiu <induhiu@hamilton.edu>
2019-06-26 12:09:42 -0800

    minor stuff

--- results.py
+++ results.py
@@ -4,10 +4,13 @@ import openpyxl as xl
 
 def main():
     ''' The main function '''
-    first = np.array(load(open('test1.txt', 'rb')))
+    # first = np.array(load(open('test1.txt', 'rb')))
     master = []
-    for i in range(2, 7):
+    for i in range(1, 3):
         master.append(np.array(load(open('test'+str(i)+'.txt', 'rb'))))
+    # for ary in master:
+        # print(ary.shape)
+    # print(master[0])
 
     # # --------------------------------------------- #
     # # Analysis
@@ -23,30 +26,31 @@ def main():
     # # Good images for the last one which has noise in it
     # print(fourth[-3], fourth[-6], fourth[-9], fourth[-12], fourth[-15], sep='\n')
 
-    print(master[4][179])
+    # print(master[4][179])
 
     # # -------------------------------------------------------------- #
     #
-    # # Writing them to an excel file for easy visualization
-    # alphabet = 'BCDEFGHIJK'
-    # wb = xl.Workbook()
-    # sheet = wb.active
-    # sheet.title = 'Test 1'
-    # for i in range(2, 6):
-    #     wb.create_sheet(title="Test"+str(i))
-    # sheets = wb.get_sheet_names()
-    # for i in range(5):
-    #     curr_sheet = wb.get_sheet_by_name(sheets[i])
-    #     for j in range(2, 182):
-    #         curr_sheet['A'+str(j)] = 'Epoch ' + str(j-1)
-    #     for j in range(10):
-    #         curr_sheet[alphabet[j]+'1'] = alphabet[j]
-    #     for j in range(180):
-    #         counter = master[i][j][0] if i >= 1 else master[i][j]
-    #         for alph in alphabet:
-    #             curr_sheet[alph + str(j+2)] = counter[alph]
-    #     print("done with sheet, ", curr_sheet)
-    # wb.save('results3.xlsx')
+    # Writing them to an excel file for easy visualization
+    alphabet = 'BCDEFGHIJK'
+    wb = xl.Workbook()
+    sheet = wb.active
+    sheet.title = 'Test 1'
+    for i in range(2, 3):
+        wb.create_sheet(title="Test"+str(i))
+    sheets = wb.get_sheet_names()
+    for i in range(2):
+        curr_sheet = wb.get_sheet_by_name(sheets[i])
+        for j in range(2, 32):
+            curr_sheet['A'+str(j)] = 'Epoch ' + str(j-1)
+        for j in range(10):
+            curr_sheet[alphabet[j]+'1'] = alphabet[j]
+        for j in range(30):
+            # counter = master[i][j][0] if i >= 1 else master[i][j]
+            counter = master[i][j][0]
+            for alph in alphabet:
+                curr_sheet[alph + str(j+2)] = counter[alph]
+        print("done with sheet, ", curr_sheet)
+    wb.save('results4.xlsx')
 
 
 if __name__ == '__main__':
