95513cc84b3222682ba4bce96725459124afcf41
induhiu <induhiu@hamilton.edu>
2019-07-12 10:53:07 -0800

    sheets

--- results.py
+++ results.py
@@ -1,46 +1,50 @@
-from pickle import load
-import numpy as np
+# from ian_gan import GAN, Generator, Discriminator
+# import pickle
 import openpyxl as xl
+import numpy as np
+
+# CITE: https://datascience.stackexchange.com/questions/9262/calculating-kl-divergence-in-python
+# DETAILS: Implementation of a KL Score
+def KL(a, b):
+    a = np.array(a, dtype=np.float)
+    b = np.array(b, dtype=np.float)
+
+    return np.sum(np.where(a != 0, a * np.log(a / b), 0))
 
 def main():
     ''' The main function '''
-    # first = np.array(load(open('test1.txt', 'rb')))
-    master = []
-    for i in range(3, 8):
-        master.append(np.array(load(open('test'+str(i)+'.txt', 'rb'))))
-    # for ary in master:
-    #     print(ary.shape)
-    # print(master[3][-5:])
-    # for _ in range(5):
-    #   print()
-    # print(master[4][-5:])
+    # vals = np.array(pickle.load(open('updated_lang_for_gan.txt', 'rb'))[:60000])
+    # vals = np.array(pickle.load(open('lang_for_gan.txt', 'rb'))[:60000])
+    # test1_lst = []
+    # test1_lst.append(gan1.train(epochs=60))
 
-    
-    # Writing them to an excel file for easy visualization
-    # alphabet = 'BCDEFGHIJK'
-    wb = xl.Workbook()
-    sheet = wb.active
-    sheet.title = 'Test 3'
-    for i in range(4, 8):
-        wb.create_sheet(title="Test"+str(i))
+    # # Test 2: Our method
+    # gan1 = GAN()
+    # disc, disc2 = Discriminator(), Discriminator()
+    # gan2 = GAN(generator=gan1.G, discriminator=disc)
+    # gan3 = GAN(generator=gan1.G, discriminator=disc2)
+    # test2_lst = []
+    # for i in range(20):
+    #     test2_lst.append(gan1.train(id=(i*3)+1))
+    #     test2_lst.append(gan2.train(id=(i*3)+2))
+    #     test2_lst.append(gan3.train(id=(i*3)+3))
+
+    # master = [test1_lst, test2_lst]
+
+    # for i in range(1, 3):
+    #     with open('test' + str(i) + '.txt', 'wb') as file:
+    #         pickle.dump(master[i - 1], file)
+
+    wb = xl.load_workbook('results5.xlsx')
     sheets = wb.get_sheet_names()
-    for i in range(5):
-        alphabet = 'BCDEFGHIJK' if i <= 2 else 'ABCDEFGHIJ'
+    cols = 'BCDEFGHIJK'
+    # a list to hold kl scores
+    kl_scores = []
+    for i in range(len(sheets)):
         curr_sheet = wb.get_sheet_by_name(sheets[i])
-        for j in range(2, 182):
-            curr_sheet['A'+str(j)] = 'Epoch ' + str(j-1)
-        for j in range(10):
-            curr_sheet['BCDEFGHIJK'[j]+'1'] = alphabet[j]
-        for j in range(180):
-            counter = master[i][j][0] if i != 3 else master[i][0][j]
-            # counter = master[i][j][0]
-            for x in range(len(alphabet)):
-                key, val = 'BCDEFGHIJK'[x] + str(j+2), counter[alphabet[x]]
-                curr_sheet[key] = val
-        print("done with sheet, ", curr_sheet)
-    wb.save('results5.xlsx')
+        vals = [(int(curr_sheet[cols[j] + str(183)].value) / 18000) for j in range(10)]
+        kl_scores.append(KL(vals, [0.1] * 10))
+    print(kl_scores)
 
 
-if __name__ == '__main__':
-    main()
-`
+main()
